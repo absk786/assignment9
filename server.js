ea22562c306239e4b401a44b62bfd6a8f6fe7d80
Abdul Rehman Khalil <abdulrehmankhalil@abduls-air.home>
2021-07-03 13:58:36 +0100

    asdfas

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const PORT = 3001;
-const HTML = fs.readFileSync('Develop/public/index.html')
 //  this tells the app to use json and utfcharacters
 app.use(express.urlencoded({ extended: true }));
 // add support for json
 app.use(express.json());
 // use the sycn verysio so no callback req. so that data is loaded before it moves tonext step.s
-const data = fs.readFileSync('develop/db/db.json');
+const data = fs.readFileSync('db/db.json');
 const notes = JSON.parse(data)
 // let getStartedBtn = getElementById("#getStartedBtn")
 console.log(notes)
@@ -21,20 +20,20 @@ app.listen(PORT, () => {
 function getStarted () { 
 console.log ("started")
 }
-app.get('/', (req, res) => {
-    res.send(HTML);
-  });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+});
 
 function addnotes () {
     // need to create a variable for the newNote that will be added
-    let newNote = "whatever user adds as the new note"
+    let newNote = "whatever user  as e new note"
     // strigify the data so we can write to the jsonfile again
     let newData = JSON.stringify(newNote);
     // function to write to the file with the call back
     fs.writeFile('develop/db/db.json', newData, finished)
     function finished () {
-        console.log('new note has been added to json file')
+        console.log('new note s been added to  file')
     }
  }
 
