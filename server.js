04f94fd34713da9db54db1608e19db84ea7d9792
Abdul Rehman Khalil <abdulrehmankhalil@abduls-air.home>
2021-07-03 13:05:08 +0100

    asdfasdf

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const PORT = 3001;
+const HTML = fs.readFileSync('Develop/public/index.html')
 //  this tells the app to use json and utfcharacters
 app.use(express.urlencoded({ extended: true }));
+// add support for json
 app.use(express.json());
 // use the sycn verysio so no callback req. so that data is loaded before it moves tonext step.s
 const data = fs.readFileSync('develop/db/db.json');
 const notes = JSON.parse(data)
+// let getStartedBtn = getElementById("#getStartedBtn")
 console.log(notes)
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
 
-function addnotes () {
+// on the click of get started I want you to get this page and load all the existing data
+function getStarted () { 
+console.log ("started")
+}
+app.get('/', (req, res) => {
+    res.send(HTML);
+  });
 
+
+function addnotes () {
     // need to create a variable for the newNote that will be added
     let newNote = "whatever user adds as the new note"
     // strigify the data so we can write to the jsonfile again
@@ -26,3 +37,5 @@ function addnotes () {
         console.log('new note has been added to json file')
     }
  }
+
+//  getStartedBtn.onclick = getStarted();
